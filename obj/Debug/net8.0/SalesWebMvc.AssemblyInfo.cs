1388bf59226720972d8df2e0f7923b58b6ba4a45
igor Morais Mendes <igormoraisone1234@outlook.com>
2025-10-29 11:13:00 -0300

    Deleting Department view and controller

--- obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091d6eb645c41192951fc2cc722ba286755857ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dc0f630ed3e1568d909a064ef3467104a80d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
