38f73fc6a7091bcbfae63385399b9026ca5e1694
igor Morais Mendes <igormoraisone1234@outlook.com>
2025-10-29 11:30:58 -0300

    CRUD Scaffolding

--- obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dc0f630ed3e1568d909a064ef3467104a80d72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1388bf59226720972d8df2e0f7923b58b6ba4a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
