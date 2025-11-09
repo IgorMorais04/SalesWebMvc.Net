c6c510b4a4d3f2471e91809e0b900e7423f143c8
igor Morais Mendes <igormoraisone1234@outlook.com>
2025-11-09 15:59:21 -0300

    SellersController

--- obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6081079b3cebebc8c597f34a7185784e6de2e7ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d0a7b0047ab2b1168a1f20653d3fb0c5bc86cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
